092124253f061518b7155815f073d82b87156af2
kylehillman <krhillman1234@gmail.com>
2021-11-21 15:41:55 -0600

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+//production
+module.exports ={
+    googleClientID: process.env.GOOGLE_CLIENT_ID, //'618035645473-9nl7ka8sdk031dfgpipdotpe02ib66ft.apps.googleusercontent.com',
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, //'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'
+    mongoURI: process.env.MONGO_URI,  //'mongodb+srv://kylehillman:Bfnd2014!@cluster0.6dj03.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',`
+    cookieKey: process.env.COOKIE_KEYV //ASDFASDGADF
+};
