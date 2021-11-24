d5f76c26d4c260288b3ec5e76256c06251f2f724
kylehillman <krhillman1234@gmail.com>
2021-11-24 14:39:50 -0600

    adding front end

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey There
+        </p>
+        <a
+          className="App-link"
+          href="http://localhost:5000/auth/google"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sign in with google
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
