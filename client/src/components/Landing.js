e3e03fdf6cb580ef4017a0e77d6f8d178bc593c8
kylehillman <krhillman1234@gmail.com>
2021-11-24 20:33:56 -0600

    continued working with front end

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return(
+        <div style ={{textAlign: 'center'}}>
+        <h1>
+        Prop Systems!
+        </h1>
+        Collect data and fly through the cloud!
+        </div>
+    );
+};
+
+export default Landing;
