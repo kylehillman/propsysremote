1598b2cf7c3f91dd21403d1b1fbfbf0a75d3b5e0
kylehillman <krhillman1234@gmail.com>
2021-11-24 21:23:39 -0600

    added body parser and some stripe routing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
-const passport = require('passport')
-const keys = require('./config/keys')
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -12,6 +13,8 @@ require('./models/User')
 require('./services/passport');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
     cookieSession({
@@ -23,7 +26,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000;
